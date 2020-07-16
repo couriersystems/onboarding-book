db6573f0760641b22d45f30275594d1937c16954
Dhruv Sringari <dhruvy427@gmail.com>
2020-07-15 23:10:09 -0400

    Ignore _book

--- _book/docs/gitbook/gitbook-plugin-remove-publish-link/rmlink.js
+++
@@ -1,6 +0,0 @@
-require([
-    'gitbook',
-    'jquery'
-],function(gitbook,$){
-    $('.gitbook-link').remove();
-})
--- _book/docs/todos.md
+++
@@ -1,3 +0,0 @@
-# To-do's
-
-This is a list of todo's an admin has to delegate or accomplish.
